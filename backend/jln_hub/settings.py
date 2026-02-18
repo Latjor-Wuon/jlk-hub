180dca715ddc0b4aa9bfb1e62cf2806481989b4b
Latjor-Wuon <l.dak@alustudent.com>
2026-02-18 19:40:35 +0200

    updated settings

--- backend/jln_hub/settings.py
+++ backend/jln_hub/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,11 +106,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'frontend' / 'src',
     BASE_DIR.parent / 'frontend',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Media files
